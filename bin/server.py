241fe0a4a4bad1d6985bb8123b7af85785ba7e93
picturestone <lukas.bildstein@outlook.com>
2019-04-03 09:43:42 +0200

    Added handler (factory to create commands)

--- bin/server.py
+++ bin/server.py
@@ -11,9 +11,10 @@ Send a HEAD request::
 Send a POST request::
     curl -d "foo=bar&bin=baz" http://localhost
 """
-from http.server import BaseHTTPRequestHandler, HTTPServer
+from http.server import BaseHTTPRequestHandler, ThreadingHTTPServer
 import socketserver,sys,json
 from time import sleep
+from handler import Handler
 
 
 class S(BaseHTTPRequestHandler):
@@ -22,30 +23,17 @@ class S(BaseHTTPRequestHandler):
         self.send_header('Content-type', 'text/html')
         self.end_headers()
 
-    def do_GET(self):
-        self._set_headers()
-        self.wfile.write("<html><body><h1>hi!</h1></body></html>")
-
-    def do_HEAD(self):
-        self._set_headers()
-
     def do_POST(self):
         # Doesn't do anything with posted data
-        print("start")
-        # sleep(10)
         length = self.headers.get('content-length')
-        requestdata = self.rfile.read(int(length)).decode("utf-8")
-        print(requestdata)
-        requestdata = requestdata.replace("\'", "\"")
+        requestdata = self.rfile.read(int(length)).decode("utf-8").replace("\'", "\"")
         requestobject = json.loads(requestdata)
-
-        print(requestobject["command"])
-        print(requestobject["parameter"]["leftwheel"])
+        handler = Handler()
+        handler.handle(requestobject)
         self._set_headers()
-        print("stop")
         self.wfile.write(b"")
 
-def run(server_class=HTTPServer, handler_class=S, port=50000):
+def run(server_class=ThreadingHTTPServer, handler_class=S, port=50000):
     server_address = ('127.0.0.1', port)
     httpd = server_class(server_address, handler_class)
     print ('Starting httpd...')
